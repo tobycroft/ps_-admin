7cbd4cd38f5ae5e7c4a6fab0f6180448112f2f6e
343195246 <30712934+343195246@users.noreply.github.com>
2022-09-01 20:13:34 +0800

    新增退出功能

---
+++ assets/user.b666a840.js
@@ -0,0 +1,1 @@
+import{p as s}from"./index.834ba41c.js";import{r as _}from"./request.57c6588b.js";const a=async e=>await _.post("https://api.ps.familyeducation.org.cn/v1/user/info/my",e),p=s("user",{state:()=>({schoolUserInfo:{}}),getters:{getSchoolUserInfo:e=>e.schoolUserInfo},actions:{async saveSchoolUserInfo(){const n=await a({});this.schoolUserInfo=n}},persist:!0});export{p as u};
