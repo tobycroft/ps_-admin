442a38416c2a6810c768ce0c172c4baaa7a85959
343195246 <30712934+343195246@users.noreply.github.com>
2022-09-08 16:13:26 +0800

    更新地址配置

---
+++ admin/assets/user.41340bc4.js
@@ -0,0 +1,1 @@
+import{p as s}from"./index.8a7a668c.js";import{r as _}from"./school.25e40e0c.js";const a=async e=>await _.post("https://api.ps.familyeducation.org.cn/v1/user/info/my",e),p=s("user",{state:()=>({schoolUserInfo:{}}),getters:{getSchoolUserInfo:e=>e.schoolUserInfo},actions:{async saveSchoolUserInfo(){const n=await a({});this.schoolUserInfo=n}},persist:!0});export{p as u};
