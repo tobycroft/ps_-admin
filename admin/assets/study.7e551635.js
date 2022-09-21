9cf4c39417ee7124673b61b3e6de7b55a2d25826
343195246 <30712934+343195246@users.noreply.github.com>
2022-09-21 16:38:42 +0800

    更新学校端:新增大屏跳转

---
+++ admin/assets/study.7e551635.js
@@ -0,0 +1,1 @@
+import{p as s}from"./index.f157a02a.js";import{t as e}from"./tag.36971158.js";const a=s("useStudyStore",{state:()=>({tagList:{}}),actions:{async tagInfoListAPI(){const t=await e({});this.tagList=t}},persist:!0});export{a as u};
---
+++ admin/assets/tag.36971158.js
@@ -0,0 +1,1 @@
+import{r as n}from"./school.1b60fd80.js";const _=async e=>await n.post("https://api.ps.familyeducation.org.cn/v1/tag/info/list",e);export{_ as t};
