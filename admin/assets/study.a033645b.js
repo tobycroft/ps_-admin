08c887cb71712bbb116e3b459705b13f830b07e0
343195246 <30712934+343195246@users.noreply.github.com>
2022-09-21 18:00:29 +0800

    测试学校端代码压缩

---
+++ admin/assets/study.a033645b.js
@@ -0,0 +1,1 @@
+import{p as s}from"./index.74846b94.js";import{t as e}from"./tag.6613979c.js";const a=s("useStudyStore",{state:()=>({tagList:{}}),actions:{async tagInfoListAPI(){const t=await e({});this.tagList=t}},persist:!0});export{a as u};
---
+++ admin/assets/user.c9bbba4f.js
@@ -0,0 +1,1 @@
+import{p as s}from"./index.74846b94.js";import{r as _}from"./school.707d21cd.js";const a=async e=>await _.post("https://api.ps.familyeducation.org.cn/v1/user/info/my",e),p=s("user",{state:()=>({schoolUserInfo:{}}),getters:{getSchoolUserInfo:e=>e.schoolUserInfo},actions:{async saveSchoolUserInfo(){const n=await a({});this.schoolUserInfo=n}},persist:!0});export{p as u};
