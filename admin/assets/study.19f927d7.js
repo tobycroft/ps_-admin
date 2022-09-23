9c4bef524a58e046809667b838956d3bc16de26e
343195246 <30712934+343195246@users.noreply.github.com>
2022-09-23 15:34:05 +0800

    全量更新动态大屏

---
+++ admin/assets/study.19f927d7.js
@@ -0,0 +1,1 @@
+import{p as s}from"./index.ea30bd78.js";import{t as e}from"./tag.3877051a.js";const a=s("useStudyStore",{state:()=>({tagList:{}}),actions:{async tagInfoListAPI(){const t=await e({});this.tagList=t}},persist:!0});export{a as u};
