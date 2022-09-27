929d963e3d8b96c66d52057f9daf9e977640f699
343195246 <30712934+343195246@users.noreply.github.com>
2022-09-27 16:47:02 +0800

    更新学校端

---
+++ admin/assets/user.fdb7ea7b.js
@@ -0,0 +1,1 @@
+import{p as s}from"./index.3b2b6dec.js";import{r as _}from"./school.1eb9c45a.js";const a=async e=>await _.post("https://api.ps.familyeducation.org.cn/v1/user/info/my",e),p=s("user",{state:()=>({schoolUserInfo:{}}),getters:{getSchoolUserInfo:e=>e.schoolUserInfo},actions:{async saveSchoolUserInfo(){const n=await a({});this.schoolUserInfo=n}},persist:!0});export{p as u};
