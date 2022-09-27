b6cb9c47e8b2a79b655b7847e2ee07811da1a9ce
343195246 <30712934+343195246@users.noreply.github.com>
2022-09-27 14:46:49 +0800

    学校端更新

---
+++ admin/assets/study.fc9bcf5d.js
@@ -0,0 +1,1 @@
+import{p as s}from"./index.22fc0091.js";import{t as e}from"./tag.089f60e1.js";const a=s("useStudyStore",{state:()=>({tagList:{}}),actions:{async tagInfoListAPI(){const t=await e({});this.tagList=t}},persist:!0});export{a as u};
