7a720f094411f6062f27274cd67b5c96ac9d0e5b
343195246 <30712934+343195246@users.noreply.github.com>
2022-10-09 18:18:55 +0800

    更新学校端

---
+++ admin/assets/study.481e2742.js
@@ -0,0 +1,1 @@
+import{p as n}from"./index.f479cc67.js";import{r as s}from"./school.4fcfc4c1.js";const _=async e=>await s.post("https://api.ps.familyeducation.org.cn/v1/tag/info/list",e),p=n("useStudyStore",{state:()=>({tagList:{}}),actions:{async tagInfoListAPI(){const e=await _({});this.tagList=e}},persist:!0});export{p as u};
---
+++ admin/assets/user.f35ef928.js
@@ -0,0 +1,1 @@
+import{p as s}from"./index.f479cc67.js";import{r as _}from"./school.4fcfc4c1.js";const a=async e=>await _.post("https://api.ps.familyeducation.org.cn/v1/user/info/my",e),p=s("user",{state:()=>({schoolUserInfo:{}}),getters:{getSchoolUserInfo:e=>e.schoolUserInfo},actions:{async saveSchoolUserInfo(){const n=await a({});this.schoolUserInfo=n}},persist:!0});export{p as u};
