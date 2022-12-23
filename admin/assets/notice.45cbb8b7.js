913580ec0bc5a6c464825d745092434aceb5f412
solojian <981298497@qq.com>
2022-12-23 18:51:30 +0800

    update admin master

---
+++ admin/assets/notice.45cbb8b7.js
@@ -0,0 +1,1 @@
+import{r as e}from"./school.75f0cc29.js";const s=async o=>await e.post("https://api.ps.familyeducation.org.cn/v1/notice/dashboard/list",o),i=async o=>await e.post("https://api.ps.familyeducation.org.cn/v1/notice/dashboard/del",o),r=async o=>await e.post("https://api.ps.familyeducation.org.cn/v1/notice/dashboard/add",o);export{i as a,r as b,s as r};
--- admin/assets/study.3007a818.js
+++
@@ -1,1 +0,0 @@
-import{p as e}from"./index.9ae69f17.js";import{r as n}from"./school.2af041d5.js";const s=async o=>await n.post("https://api.ps.familyeducation.org.cn/v1/tag/info/list",o),m=e("useStudyStore",{state:()=>({tagList:{}}),actions:{async tagInfoListAPI(){const o=await s({});this.tagList=o}},persist:!0});export{m as u};
