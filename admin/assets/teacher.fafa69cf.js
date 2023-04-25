d7d8ee38a70281911a36490507dd63476987e601
火线兔 <silverhawk@foxmail.com>
2023-04-25 16:09:26 +0800

    TuuzMac

--- admin/assets/teacher.fafa69cf.js
+++
@@ -1,1 +0,0 @@
-import{p as a}from"./index.44de20e8.js";import{r}from"./datacenter.c5f25cab.js";const i=a("teacher",{state:()=>({teacherList:{}}),getters:{getTeacherList:e=>e.teacherList},actions:{async reqDatasourceTeacherAPI(e){const t=await r({school_id:e});this.teacherList=t}},persist:!0});export{i as u};
--- admin/assets/user.05d2d1f6.js
+++
@@ -1,1 +0,0 @@
-import{p as s}from"./index.44de20e8.js";import{r as _}from"./request.5b120ebb.js";const i=async e=>await _.post("https://api.ps.familyeducation.org.cn/v1/user/info/my",e),a=s("user",{state:()=>({schoolUserInfo:{}}),getters:{getSchoolUserInfo:e=>e.schoolUserInfo},actions:{async saveSchoolUserInfo(){const n=await i({});this.schoolUserInfo=n}},persist:!0});export{a as u};
