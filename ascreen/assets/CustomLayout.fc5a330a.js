d8b3a338bed603a6f8ee8cd745249adf1ad5b06d
火线兔 <silverhawk@foxmail.com>
2023-05-05 10:22:09 +0800

    TuuzMac

---
+++ ascreen/assets/CustomLayout.fc5a330a.js
@@ -0,0 +1,1 @@
+import{_ as o,R as t,a as s,r as a,o as n,c as _,b as c}from"./index.4ce35190.js";const r={name:"CustomLayout",setup(){return{RouterLink:t,RouterView:s}}},u={class:"custom-layout-container"};function p(i,d,m,l,f,y){const e=a("RouterView");return n(),_("div",u,[c(e)])}const R=o(r,[["render",p],["__scopeId","data-v-f7593ee4"]]);export{R as default};
