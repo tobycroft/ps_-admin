6cad618213d556696e451a5aaba5c6e0047e2d07
yqs2021 <1814926166@qq.com>
2023-05-08 13:55:25 +0800

    33

--- admin/assets/study.c9523294.js
+++
@@ -1,1 +0,0 @@
-import{p as n}from"./index.459154b7.js";import{r as _}from"./request.0caaa926.js";const s=async e=>await _.post("https://api.ps.familyeducation.org.cn/v1/tag/info/list",e),p=n("useStudyStore",{state:()=>({tagList:{}}),actions:{async tagInfoListAPI(){const e=await s({});this.tagList=e}},persist:!0});export{p as u};
