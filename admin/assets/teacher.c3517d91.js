f45d277e66e2084545c46c03a57345a0e118a823
yqs2021 <1814926166@qq.com>
2023-05-08 18:06:23 +0800

    家长排行、班级排行

--- admin/assets/teacher.c3517d91.js
+++
@@ -1,1 +0,0 @@
-import{p as a}from"./index.df39f28b.js";import{r}from"./datacenter.73092ee7.js";const i=a("teacher",{state:()=>({teacherList:{}}),getters:{getTeacherList:e=>e.teacherList},actions:{async reqDatasourceTeacherAPI(e){const t=await r({school_id:e});this.teacherList=t}},persist:!0});export{i as u};
