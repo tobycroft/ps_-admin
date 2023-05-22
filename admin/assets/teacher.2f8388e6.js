9a10a694880279354cc3ef0159e8eeaa7366aba1
yqs2021 <1814926166@qq.com>
2023-05-22 11:34:42 +0800

    学分排行-显示条数

--- admin/assets/teacher.2f8388e6.js
+++
@@ -1,1 +0,0 @@
-import{p as a}from"./index.4f62021f.js";import{r}from"./datacenter.f605ded7.js";const i=a("teacher",{state:()=>({teacherList:{}}),getters:{getTeacherList:e=>e.teacherList},actions:{async reqDatasourceTeacherAPI(e){const t=await r({school_id:e});this.teacherList=t}},persist:!0});export{i as u};
