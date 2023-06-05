d8bcc1c30ab37cb2e0b85d6ab93d16302878120f
yqs2022 <1814926166@qq.com>
2023-06-05 16:07:29 +0800

    测试2

---
+++ admin/assets/teacher.1b50eebb.js
@@ -0,0 +1,1 @@
+import{p as a}from"./index.ad456479.js";import{r}from"./datacenter.e8aaac8a.js";const i=a("teacher",{state:()=>({teacherList:{}}),getters:{getTeacherList:e=>e.teacherList},actions:{async reqDatasourceTeacherAPI(e){const t=await r({school_id:e});this.teacherList=t}},persist:!0});export{i as u};
