226c9af22312a8d5b675f58495b5535b85e676e7
yqs2022 <1814926166@qq.com>
2023-06-07 16:32:51 +0800

    切换时间闭合年纪各班详情

---
+++ admin/assets/study.5f711e6f.js
@@ -0,0 +1,1 @@
+import{p as n}from"./index.518dc570.js";import{r as s}from"./request.81da71e9.js";const _=async e=>await s.post("https://api.ps.familyeducation.org.cn/v1/tag/info/list",e),p=n("useStudyStore",{state:()=>({tagList:{}}),actions:{async tagInfoListAPI(){const e=await _({});this.tagList=e}},persist:!0});export{p as u};
--- admin/assets/study.9fe445d9.js
+++
@@ -1,1 +0,0 @@
-import{p as n}from"./index.92f98db6.js";import{r as s}from"./request.bb53f7b7.js";const _=async e=>await s.post("https://api.ps.familyeducation.org.cn/v1/tag/info/list",e),p=n("useStudyStore",{state:()=>({tagList:{}}),actions:{async tagInfoListAPI(){const e=await _({});this.tagList=e}},persist:!0});export{p as u};
