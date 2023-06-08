ee21effa2e63f24bcab3beb15f21715df3ed58cb
yqs2022 <1814926166@qq.com>
2023-06-08 11:45:33 +0800

    榜位评价文字更换图片

--- admin/assets/teacher.c63140fa.js
+++
@@ -1,1 +0,0 @@
-import{p as a}from"./index.518dc570.js";import{r}from"./datacenter.a54c9ed0.js";const i=a("teacher",{state:()=>({teacherList:{}}),getters:{getTeacherList:e=>e.teacherList},actions:{async reqDatasourceTeacherAPI(e){const t=await r({school_id:e});this.teacherList=t}},persist:!0});export{i as u};
--- admin/assets/user.467b77f0.js
+++
@@ -1,1 +0,0 @@
-import{p as s}from"./index.518dc570.js";import{r as _}from"./request.81da71e9.js";const i=async e=>await _.post("https://api.ps.familyeducation.org.cn/v1/user/info/my",e),a=s("user",{state:()=>({schoolUserInfo:{}}),getters:{getSchoolUserInfo:e=>e.schoolUserInfo},actions:{async saveSchoolUserInfo(){const n=await i({});this.schoolUserInfo=n}},persist:!0});export{a as u};
