008ad548adefd008cf35271e3afc1cb81115871c
yqs2022 <1814926166@qq.com>
2023-07-07 11:20:34 +0800

    海报、小中大班

---
+++ admin/assets/teacher.60437dc8.js
@@ -0,0 +1,1 @@
+import{p as a}from"./index.6cb4e5d4.js";import{r}from"./datacenter.bd5d85dd.js";const i=a("teacher",{state:()=>({teacherList:{}}),getters:{getTeacherList:e=>e.teacherList},actions:{async reqDatasourceTeacherAPI(e){const t=await r({school_id:e});this.teacherList=t}},persist:!0});export{i as u};
