57ca50e64917a81fa6de8f9c5497b1320c28e28b
yqs2022 <1814926166@qq.com>
2023-07-07 14:26:15 +0800

    文章详情-所属年级

---
+++ admin/assets/teacher.4a0947e8.js
@@ -0,0 +1,1 @@
+import{p as a}from"./index.df8d7c83.js";import{r}from"./datacenter.664c2e8a.js";const i=a("teacher",{state:()=>({teacherList:{}}),getters:{getTeacherList:e=>e.teacherList},actions:{async reqDatasourceTeacherAPI(e){const t=await r({school_id:e});this.teacherList=t}},persist:!0});export{i as u};
---
+++ admin/assets/user.71d897ca.js
@@ -0,0 +1,1 @@
+import{p as s}from"./index.df8d7c83.js";import{r as _}from"./request.6122a044.js";const i=async e=>await _.post("https://api.ps.familyeducation.org.cn/v1/user/info/my",e),p=s("user",{state:()=>({schoolUserInfo:{}}),getters:{getSchoolUserInfo:e=>e.schoolUserInfo},actions:{async saveSchoolUserInfo(){const n=await i({});this.schoolUserInfo=n}},persist:!0});export{p as u};
