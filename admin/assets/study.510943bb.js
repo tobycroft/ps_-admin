c1b41a49c7dff4e600a484cd4a1a7a871468962e
yqs2022 <1814926166@qq.com>
2023-07-07 11:38:17 +0800

    微调

---
+++ admin/assets/study.510943bb.js
@@ -0,0 +1,1 @@
+import{p as n}from"./index.099d31f6.js";import{r as s}from"./request.4cdd08ea.js";const _=async e=>await s.post("https://api.ps.familyeducation.org.cn/v1/tag/info/list",e),a=n("useStudyStore",{state:()=>({tagList:{}}),actions:{async tagInfoListAPI(){const e=await _({});this.tagList=e}},persist:!0});export{a as u};
