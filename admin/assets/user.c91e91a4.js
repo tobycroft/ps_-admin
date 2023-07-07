4a9e168588b36186a8762f2d702e4bf60acb97a0
yqs2022 <1814926166@qq.com>
2023-07-07 15:16:55 +0800

    海报-班级、时间格式

---
+++ admin/assets/user.c91e91a4.js
@@ -0,0 +1,1 @@
+import{p as s}from"./index.c2e837e5.js";import{r as _}from"./request.c6b79b89.js";const i=async e=>await _.post("https://api.ps.familyeducation.org.cn/v1/user/info/my",e),p=s("user",{state:()=>({schoolUserInfo:{}}),getters:{getSchoolUserInfo:e=>e.schoolUserInfo},actions:{async saveSchoolUserInfo(){const n=await i({});this.schoolUserInfo=n}},persist:!0});export{p as u};
