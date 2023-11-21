0119b15d315797953d57df668ba62dce48e66316
zhangdaofu <zhangdaofu@zhiyuan.com>
2023-11-21 14:09:04 +0800

    修改年级

---
+++ screen/assets/CustomLayout.6089cad5.js
@@ -0,0 +1,1 @@
+import{_ as o,R as t,a as s,r as a,o as c,c as n,b as _}from"./index.2640e79c.js";const r={name:"CustomLayout",setup(){return{RouterLink:t,RouterView:s}}},u={class:"custom-layout-container"};function p(i,d,m,l,f,y){const e=a("RouterView");return c(),n("div",u,[_(e)])}const R=o(r,[["render",p],["__scopeId","data-v-3455cb21"]]);export{R as default};
