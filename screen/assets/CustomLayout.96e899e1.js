3251b848eee0fd70184d2f3c278b12e9527a90af
zhangdaofu <zhangdaofu@zhiyuan.com>
2023-11-21 14:23:26 +0800

    utc时区修正

---
+++ screen/assets/CustomLayout.96e899e1.js
@@ -0,0 +1,1 @@
+import{_ as o,R as t,a as s,r as a,o as c,c as n,b as _}from"./index.5ea294d7.js";const r={name:"CustomLayout",setup(){return{RouterLink:t,RouterView:s}}},u={class:"custom-layout-container"};function p(i,d,m,l,f,y){const e=a("RouterView");return c(),n("div",u,[_(e)])}const R=o(r,[["render",p],["__scopeId","data-v-3455cb21"]]);export{R as default};
