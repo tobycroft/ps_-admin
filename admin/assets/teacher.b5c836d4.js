b2f606a402ca788928644c41c40742110f8334ac
zhangdaofu <zhangdaofu@zhiyuan.com>
2023-12-05 14:29:03 +0800

    大屏跳转链接修改

---
+++ admin/assets/teacher.b5c836d4.js
@@ -0,0 +1,1 @@
+import{p as a}from"./index.2f82ed88.js";import{r}from"./datacenter.d03e62dc.js";const i=a("teacher",{state:()=>({teacherList:{}}),getters:{getTeacherList:e=>e.teacherList},actions:{async reqDatasourceTeacherAPI(e){const t=await r({school_id:e});this.teacherList=t}},persist:!0});export{i as u};
