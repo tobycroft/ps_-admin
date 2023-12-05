874686cdfdaf49f12fbeb2245ba2efb2822bcb33
zhangdaofu <zhangdaofu@zhiyuan.com>
2023-12-05 14:12:31 +0800

    UI调整，切换uni分支

--- screen/assets/CustomLayout.3e6468cd.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,R as t,a as s,r as a,o as c,c as n,b as _}from"./index.04d76fc0.js";const r={name:"CustomLayout",setup(){return{RouterLink:t,RouterView:s}}},u={class:"custom-layout-container"};function p(i,d,m,l,f,y){const e=a("RouterView");return c(),n("div",u,[_(e)])}const R=o(r,[["render",p],["__scopeId","data-v-3455cb21"]]);export{R as default};
