101f571ac7483b0412232b86bd7a61449c2652f0
zhangdaofu <zhangdaofu@zhiyuan.com>
2024-01-04 15:41:06 +0800

    家委转发贡献样式修改

---
+++ admin/assets/teacher.c675a68f.js
@@ -0,0 +1,1 @@
+import{p as a}from"./index.ae89ba5c.js";import{r}from"./datacenter.dc8d1348.js";const i=a("teacher",{state:()=>({teacherList:{}}),getters:{getTeacherList:e=>e.teacherList},actions:{async reqDatasourceTeacherAPI(e){const t=await r({school_id:e});this.teacherList=t}},persist:!0});export{i as u};
---
+++ admin/assets/user.c27333f4.js
@@ -0,0 +1,1 @@
+import{p as n}from"./index.ae89ba5c.js";import{r as s}from"./request.43a4cd59.js";const i=async o=>await s.post("https://api.ps.familyeducation.org.cn/v1/user/info/my",o),_=n("user",{state:()=>({schoolUserInfo:{}}),getters:{getSchoolUserInfo:o=>o.schoolUserInfo},actions:{async saveSchoolUserInfo(){const e=await i({});this.schoolUserInfo=e}},persist:!0});export{_ as u};
