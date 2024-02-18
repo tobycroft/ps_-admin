dc65c40a5c2b76457ae30662e3ed2a4ad163a10f
zhangdaofu <zhangdaofu@zhiyuan.com>
2024-02-18 17:12:03 +0800

    /admin/studyOverview dateFormat

---
+++ admin/assets/teacher.f9c6b7a9.js
@@ -0,0 +1,1 @@
+import{p as a}from"./index.b8cfcbe6.js";import{r}from"./datacenter.3daea5e6.js";const i=a("teacher",{state:()=>({teacherList:{}}),getters:{getTeacherList:e=>e.teacherList},actions:{async reqDatasourceTeacherAPI(e){const t=await r({school_id:e});this.teacherList=t}},persist:!0});export{i as u};
