7d99a1059518eec80150cd67a3a88498d3bbaecb
zhangdaofu <zhangdaofu@zhiyuan.com>
2024-02-19 11:11:11 +0800

    study/info/list增加end_date参数

---
+++ admin/assets/study.80f0bd48.js
@@ -0,0 +1,1 @@
+import{p as e}from"./index.14898826.js";import{r as n}from"./request.ebae3eec.js";const s=async o=>await n.post("https://api.ps.familyeducation.org.cn/v1/tag/info/list",o),a=e("useStudyStore",{state:()=>({tagList:{}}),actions:{async tagInfoListAPI(){const o=await s({});this.tagList=o}},persist:!0});export{a as u};
---
+++ admin/assets/teacher.0c76d50c.js
@@ -0,0 +1,1 @@
+import{p as a}from"./index.14898826.js";import{r}from"./datacenter.b47e6385.js";const i=a("teacher",{state:()=>({teacherList:{}}),getters:{getTeacherList:e=>e.teacherList},actions:{async reqDatasourceTeacherAPI(e){const t=await r({school_id:e});this.teacherList=t}},persist:!0});export{i as u};
