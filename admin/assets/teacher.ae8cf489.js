5ddba56e4b4d01361582d8b6061bc55fbb926cb9
zhangdaofu <zhangdaofu@zhiyuan.com>
2024-02-20 15:49:56 +0800

    课程概览：增加end_date入参

---
+++ admin/assets/teacher.ae8cf489.js
@@ -0,0 +1,1 @@
+import{p as a}from"./index.f5538129.js";import{r}from"./datacenter.20e2b2e3.js";const i=a("teacher",{state:()=>({teacherList:{}}),getters:{getTeacherList:e=>e.teacherList},actions:{async reqDatasourceTeacherAPI(e){const t=await r({school_id:e});this.teacherList=t}},persist:!0});export{i as u};
---
+++ admin/assets/user.90feef74.js
@@ -0,0 +1,1 @@
+import{p as n}from"./index.f5538129.js";import{r as s}from"./request.f453e928.js";const i=async o=>await s.post("https://api.ps.familyeducation.org.cn/v1/user/info/my",o),_=n("user",{state:()=>({schoolUserInfo:{}}),getters:{getSchoolUserInfo:o=>o.schoolUserInfo},actions:{async saveSchoolUserInfo(){const e=await i({});this.schoolUserInfo=e}},persist:!0});export{_ as u};
