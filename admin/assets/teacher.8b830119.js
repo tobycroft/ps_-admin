72f7598b5271a75a8f0ce6c2efbc9afb135c816e
zhangdaofu <zhangdaofu@zhiyuan.com>
2024-02-21 14:30:17 +0800

    海报页面增加loading

---
+++ admin/assets/teacher.8b830119.js
@@ -0,0 +1,1 @@
+import{p as a}from"./index.8b62e2d4.js";import{r}from"./datacenter.51803e39.js";const i=a("teacher",{state:()=>({teacherList:{}}),getters:{getTeacherList:e=>e.teacherList},actions:{async reqDatasourceTeacherAPI(e){const t=await r({school_id:e});this.teacherList=t}},persist:!0});export{i as u};
