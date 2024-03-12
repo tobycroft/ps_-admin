5af5c75fa60b79ecfe3d2fff18837b73a3cabca1
zhangdaofu <zhangdaofu@zhiyuan.com>
2024-03-12 14:53:48 +0800

    文本修改为总学习量

---
+++ admin/assets/teacher.3658e34e.js
@@ -0,0 +1,1 @@
+import{p as a}from"./index.6ec564e4.js";import{r}from"./datacenter.f41f5a73.js";const i=a("teacher",{state:()=>({teacherList:{}}),getters:{getTeacherList:e=>e.teacherList},actions:{async reqDatasourceTeacherAPI(e){const t=await r({school_id:e});this.teacherList=t}},persist:!0});export{i as u};
